79c5ece2bbc8143c5a2d033a5427c14a023e2546
lord-ashborn <yedukrish88@gmail.com>
2026-09-22 06:11:11 +0530

    Update labsheet exercises

---
+++ ComplexNumber.java
@@ -0,0 +1,19 @@
+class ComplexNumber { 
+    int real; 
+    int imaginary; 
+    ComplexNumber(int real, int imaginary) { 
+        this.real = real; 
+        this.imaginary = imaginary; 
+    } 
+    void add(ComplexNumber c) { 
+        int realSum = this.real + c.real; 
+        int imaginarySum = this.imaginary + c.imaginary; 
+        System.out.println("Sum = " + realSum + " + " + imaginarySum + "i"); 
+
+    } 
+    public static void main(String[] args) { 
+        ComplexNumber c1 = new ComplexNumber(4, 5); 
+        ComplexNumber c2 = new ComplexNumber(3, 2); 
+        c1.add(c2); 
+    } 
+} 
